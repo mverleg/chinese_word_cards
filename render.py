009c14179ebbf3e5ae1569d8367339dd8192f57a
Mark <mverleg.noreply@gmail.com>
2019-12-04 22:31:57 +0100

    Pagination

--- render.py
+++ render.py
@@ -12,16 +12,20 @@ def main(args):
     with open('words.csv', 'r') as fh:
         lines = fh.read().splitlines()
     html = ["<!DOCTYPE html><html><body>"]
-    word_blocks = []
-    current_block = []
+    pages = []
+    current_cards = []
+    current_words = []
     for line in lines:
         if ';' not in line:
             continue
-        current_block.append(line)
-        if len(current_block) >= 6:
-            word_blocks.append(current_block)
-            current_block = []
-    print(word_blocks)
+        current_words.append(line)
+        if len(current_words) >= 6:
+            current_cards.append(current_words)
+            current_words = []
+            if len(current_cards) >= 9:
+                pages.append(current_cards)
+                current_cards = []
+    print(pages)
     html.append("</body></html>")
 
 
